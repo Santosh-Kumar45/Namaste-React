d90e394b6a1fb9be209db2808c8f8a1db68a710d
santosh-kumar45 <ksantosh04004@gmail.com>
2026-06-04 20:34:54 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,7 @@
+const parent = React.createElement("div",{id:"parent"},
+    React.createElement("div",{id:"child"},
+        React.createElement("h1",{},"first h1 heading")
+    )
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
